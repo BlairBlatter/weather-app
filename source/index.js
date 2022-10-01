6ff0aad5dbef8c8bfa868d52c280c56ea8bfd707
BlairBlatter <blairblatter@gmail.com>
2022-10-01 15:45:39 -0500

    fixed spelling error

--- source/index.js
+++ source/index.js
@@ -60,7 +60,7 @@ function getForecast(coordinates) {
   let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
   let units = "imperial";
   let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
-  axois.ger(apiUrl).then(displayForecast);
+  axois.get(apiUrl).then(displayForecast);
 }
 
 function displayWeather(response) {
